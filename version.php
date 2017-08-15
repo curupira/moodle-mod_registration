5e86b146c52352a16f54342131caba8e20417f27
Justus Flerlage <flerlage@math.tu-berlin.de>
2017-08-15 10:53:00 +0000

    updated version information

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 defined("MOODLE_INTERNAL") || die();
 
 $plugin->component = "mod_registration";
-$plugin->version   = 2017070601;
-$plugin->release   = 'v2.1.3';
+$plugin->version   = 2017081501;
+$plugin->release   = 'v2.1.4';
 $plugin->requires  = 2016120503;
 $plugin->cron      = 60;
 
